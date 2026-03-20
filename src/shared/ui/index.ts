7c018b6193cae0d824d5f3ef268dc3179f6a1793
Tshepang <tshepangmog10@gmail.com>
2026-03-20 18:19:37 +0200

    feat(shared): add ui primitives and shared utility exports

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,26 @@
+export { Badge } from "./badge";
+export { Button, buttonVariants } from "./button";
+export {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "./card";
+export {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogOverlay,
+	DialogPortal,
+	DialogTitle,
+	DialogTrigger,
+} from "./dialog";
+export { Input } from "./input";
+export { Separator } from "./separator";
+export { Slider } from "./slider";
