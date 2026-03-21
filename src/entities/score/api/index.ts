02bd4b076e356fd5e3af227e553f356331287b47
Tshepang <tshepangmog10@gmail.com>
2026-03-21 20:54:47 +0200

    feat(score-api): add convex leaderboard query factory

--- src/entities/score/api/index.ts
+++ src/entities/score/api/index.ts
@@ -1,1 +1,2 @@
-export {};
+export { scoreQueries } from "./queries";
+export { SCORE_QUERY_KEYS } from "./queryKeys";
--- src/entities/score/index.ts
+++ src/entities/score/index.ts
@@ -1,1 +1,2 @@
+export { SCORE_QUERY_KEYS, scoreQueries } from "./api";
 export type { ScoreEntry, ScoreSubmission } from "./model/types";
