84ad16eb1ff0f19a7dae760dc35a8c7e049b9907
Tshepang <tshepangmog10@gmail.com>
2026-04-23 19:30:42 +0200

    feat(settings-panel): add panel view-model hook and config ids

---
+++ src/widgets/settings-panel/config/settingsPanelConfig.ts
@@ -0,0 +1,9 @@
+export const SETTINGS_PANEL_IDS = {
+	AUDIO_SECTION: "audio-settings-heading",
+	GRAPHICS_SECTION: "graphics-settings-heading",
+	HAPTICS_SECTION: "haptics-settings-heading",
+	MASTER_VOLUME_DESCRIPTION: "master-volume-description",
+	MASTER_VOLUME_LABEL: "master-volume-label",
+	MUSIC_VOLUME_DESCRIPTION: "music-volume-description",
+	MUSIC_VOLUME_LABEL: "music-volume-label",
+} as const;
