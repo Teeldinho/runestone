822f46dec447ea4cf3ed1b8c6d5ed3ba3b4f8fe2
Tshepang <tshepangmog10@gmail.com>
2026-05-25 18:45:11 +0200

    feat(marketing-shell): add navigation config and model

---
+++ src/widgets/marketing-shell/config/index.ts
@@ -0,0 +1,8 @@
+export {
+	MARKETING_FOOTER_LINKS,
+	MARKETING_NAVIGATION_ITEM_IDS,
+	MARKETING_NAVIGATION_ITEMS,
+	MARKETING_ROUTES,
+	MARKETING_SHELL_COPY,
+	type MarketingNavigationItemId,
+} from "./marketingShellConfig";
