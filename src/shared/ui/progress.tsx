e9c4220cb213e0076583c975da7e35ca6fcf12c9
Tshepang <tshepangmog10@gmail.com>
2026-06-06 13:18:47 +0200

    chore(tooling): add ast-grep structural lint

--- src/shared/ui/progress.tsx
+++ src/shared/ui/progress.tsx
@@ -1,7 +1,7 @@
 import { Progress as ProgressPrimitive } from "radix-ui";
 import type * as React from "react";
 
-import { cn } from "@/shared/lib/utils";
+import { cn } from "@/shared/lib";
 
 function Progress({
 	className,
